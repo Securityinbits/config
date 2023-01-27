658ec170d86146b945b76a753939a8a5eb7c931d
Ayush Anand <34771151+Securityinbits@users.noreply.github.com>
2023-01-27 15:47:56 +0800

    Powershell profile

---
+++ Profile.ps1
@@ -0,0 +1,7 @@
+### PowerShell template profile 
+### Version 1.00
+### Current User, All Hosts - Windows - $HOME\Documents\PowerShell\Profile.ps1
+### This file should be stored in $PROFILE.CurrentUserAllHosts
+### If $PROFILE.CurrentUserAllHosts doesn't exist, you can make one with the following:
+### New-Item -ItemType File -Path $PROFILE.CurrentUserAllHosts -Force
+### This will create the file and the containing subdirectory if it doesn't already 
