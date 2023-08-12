3ba899ca170b6e522faeef14e9a962449d25fc73
Ayush Anand <34771151+Securityinbits@users.noreply.github.com>
2023-08-12 17:35:17 +0800

    added some more comments to load in mac

--- Profile.ps1
+++ Profile.ps1
@@ -2,7 +2,11 @@
 ### Version 1.00
 ### 
 ### Current User, All Hosts - Windows - $HOME\Documents\PowerShell\Profile.ps1
-### This file should be stored in $PROFILE.CurrentUserAllHosts
+
+### This file should be stored in $PROFILE.CurrentUserAllHosts 
+### For Mac/Linux just do vim $PROFILE.CurrentUserAllHosts  to edit the file 
+#### To relaod the profile just do . $PROFILE.CurrentUserAllHosts
+
 ### If $PROFILE.CurrentUserAllHosts doesn't exist, you can make one with the following:
 ### New-Item -ItemType File -Path $PROFILE.CurrentUserAllHosts -Force
 ### This will create the file and the containing subdirectory if it doesn't already
